c93a2ab1fa55c7d538e1023ad97ac1792c19baf4
acse-moj20 <moj20@ic.ac.uk>
2021-02-17 13:17:44 +0000

    blocking communication exercises

---
+++ exercise1_2.cpp
@@ -0,0 +1,82 @@
+#include <mpi.h>
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+int id, p;
+
+// Lecture 1 Exercise 2: Ring Communication w/Probe 
+// ======================================
+int exercise2(int argc, char* argv[])
+{
+
+
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	srand(time(NULL) + id * 10);	
+
+	
+	int tag_num = 1;
+
+	if (id == 0) {
+		// data to send 
+
+		int num_recv;
+		int num_send = 1 + rand() % 3;
+		int* data = new int[num_send];
+
+		for (int j = 0; j < num_send; j++) { data[j] = rand(); }
+
+		MPI_Send(data, num_send, MPI_INT, id + 1, tag_num, MPI_COMM_WORLD);
+		cout << "Processor " << id << " added: ";
+		for (int i = 0; i != num_send; i++) {
+			cout << data[i] << " ";
+		}
+		cout << endl;
+
+		MPI_Status status;
+		MPI_Probe(p - 1, tag_num, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, MPI_INT, &num_recv);
+		MPI_Recv(data, num_recv, MPI_INT, p - 1, tag_num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		cout << "Reciving all these info back: ";
+		for (int j = 0; j != num_recv; j++) {
+			cout << data[j] << " ";
+		}
+		cout << " to processor " << id << ".";
+		tag_num++;
+
+		delete[] data;
+	}
+	else {
+		int id_recv = id - 1;
+		int id_send = (id + 1) % p;
+		int num_recv;
+		int num_send = 1 + rand() % 3;
+
+		MPI_Status status;
+		MPI_Probe(id_recv, tag_num, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, MPI_INT, &num_recv);
+		int* data = new int[num_recv];
+		MPI_Recv(data, num_recv, MPI_INT, id_recv, tag_num, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+
+		for (int j = num_recv; j < num_recv + num_send; j++) { data[j] = rand(); }
+
+		MPI_Send(data, num_recv + num_send, MPI_INT, id_send, tag_num, MPI_COMM_WORLD);
+		cout << "Processor " << id << " added: ";
+		for (int i = num_recv; i != num_recv + num_send; i++) {
+			cout << data[i] << " ";
+		}
+		cout << endl;
+		tag_num++;
+
+		delete[] data;
+
+	}
+
+
+	MPI_Finalize();
+}
---
+++ exercise1_3.cpp
@@ -0,0 +1,60 @@
+#include <mpi.h>
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+int id, p;
+
+// Lecture 1 Exercise 3 : Blocking Point to Point Communication
+// ==================================================
+int main(int argc, char* argv[])
+{
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	srand(time(NULL) + id * 10);
+
+	int tag_num = 1;
+
+	if (id == 0) {
+		// data to send
+		int* data = new int[p];
+		data[0] = rand();
+
+		MPI_Send(data, 1, MPI_INT, id + 1, tag_num, MPI_COMM_WORLD);
+
+
+		MPI_Recv(data, p, MPI_INT, p - 1, tag_num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		cout << "Receiving all these back: ";
+		for (int j = 0; j != p; j++) {
+			cout << data[j] << "\t";
+		}
+		cout << " to processor: " << id << ".";
+		tag_num++;
+
+		delete[] data;
+	}
+	else {
+		int id_recv = id - 1;
+		int id_send = (id + 1) % p;
+		int *data = new int [id];
+		MPI_Recv(data, id, MPI_INT, id_recv, tag_num, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+
+		cout << "Receiving " << data[id -1] << " on processor: " << id << endl;
+
+
+		data[id] = rand();
+
+		MPI_Send(data, id + 1, MPI_INT, id_send, tag_num, MPI_COMM_WORLD);
+		cout << "Sending " << data[id] << " to processor: " << id_send << endl;
+		tag_num++;
+
+		delete[] data;
+
+	}
+
+	MPI_Finalize();
+}
