db21af17afc44bca9fdcf0326747dff4eba48b76
acse-moj20 <moj20@ic.ac.uk>
2021-03-12 11:40:29 +0000

    completed part 2 of exercise

--- exercise1_2.cpp
+++ exercise1_2.cpp
@@ -1,15 +1,26 @@
 #include <mpi.h>
 #include <iostream>
+#include <cstdlib>
 #include <time.h>
 #include <vector>
 
-using namespace std;
+/*
+ Lecture 1 Exercise 2: Ring Communication w/Probe 
+ ================================================
 
+ Modify the Ring Communication program from exercise 1 so that each process 
+ randomly adds between 1 and 3 new numbers to the list being passed around the ring. 
+ You should probe for the size of the data to be received, allocate enough memory to 
+ store both this data and the new data to be added to the list, receive this data, 
+ append the new data to the end of the dynamically allocated array and then send this
+ data on to the next processor. Remember to free the memory after using it.
+
+*/
+
+using namespace std;
 int id, p;
 
-// Lecture 1 Exercise 2: Ring Communication w/Probe 
-// ================================================
-int exercise1_2(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
@@ -18,29 +29,31 @@ int exercise1_2(int argc, char* argv[])
 
 	
 	int tag_num = 1;
+	int* data;
 
 	if (id == 0) {
 		// data to send 
 
-		int num_recv;
-		int num_send = 1 + rand() % 3;
-		int* data = new int[num_send];
-
-		for (int j = 0; j < num_send; j++) { data[j] = rand(); }
-
-		MPI_Send(data, num_send, MPI_INT, id + 1, tag_num, MPI_COMM_WORLD);
+		int recv_count;
+		int send_count = 1 + rand() % 3;
+		data = new int[send_count];
 		cout << "Processor " << id << " added: ";
-		for (int i = 0; i != num_send; i++) {
-			cout << data[i] << " ";
+		for (int j = 0; j < send_count; j++) { 
+			data[j] = rand(); 
+			cout << data[j] << ' ';
 		}
 		cout << endl;
+		MPI_Send(data, send_count, MPI_INT, id + 1, tag_num, MPI_COMM_WORLD);
+		delete[] data;
 
 		MPI_Status status;
 		MPI_Probe(p - 1, tag_num, MPI_COMM_WORLD, &status);		// Probe 
-		MPI_Get_count(&status, MPI_INT, &num_recv);
-		MPI_Recv(data, num_recv, MPI_INT, p - 1, tag_num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Get_count(&status, MPI_INT, &recv_count);
+		data = new int[recv_count];
+		MPI_Recv(data, recv_count, MPI_INT, p - 1, tag_num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		
 		cout << "Receiving all these info back: ";
-		for (int j = 0; j != num_recv; j++) {
+		for (int j = 0; j != recv_count; j++) {
 			cout << data[j] << " ";
 		}
 		cout << " to processor " << id << ".";
@@ -51,20 +64,21 @@ int exercise1_2(int argc, char* argv[])
 	else {
 		int id_recv = id - 1;
 		int id_send = (id + 1) % p;
-		int num_recv;
-		int num_send = 1 + rand() % 3;
+		int recv_count;
+		int send_count = 1 + rand() % 3; // additional data sent by processor
 
 		MPI_Status status;
 		MPI_Probe(id_recv, tag_num, MPI_COMM_WORLD, &status);
-		MPI_Get_count(&status, MPI_INT, &num_recv);
-		int* data = new int[num_recv];
-		MPI_Recv(data, num_recv, MPI_INT, id_recv, tag_num, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		MPI_Get_count(&status, MPI_INT, &recv_count);
 
-		for (int j = num_recv; j < num_recv + num_send; j++) { data[j] = rand(); }
+		data = new int[recv_count + send_count];
+		MPI_Recv(data, recv_count, MPI_INT, id_recv, tag_num, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+		for (int j = recv_count; j < recv_count + send_count; j++) { 
+			data[j] = rand(); }
 
-		MPI_Send(data, num_recv + num_send, MPI_INT, id_send, tag_num, MPI_COMM_WORLD);
+		MPI_Send(data, recv_count + send_count, MPI_INT, id_send, tag_num, MPI_COMM_WORLD);
 		cout << "Processor " << id << " added: ";
-		for (int i = num_recv; i != num_recv + num_send; i++) {
+		for (int i = recv_count; i != recv_count + send_count; i++) {
 			cout << data[i] << " ";
 		}
 		cout << endl;
@@ -76,4 +90,6 @@ int exercise1_2(int argc, char* argv[])
 
 
 	MPI_Finalize();
+
+	return 0;
 }
