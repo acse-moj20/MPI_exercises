00d4de45d00b34f69fd2da94be414fa05015c252
acse-moj20 <moj20@ic.ac.uk>
2021-03-30 17:09:11 +0100

    rename for better id

--- blocking_p2p.cpp
+++ blocking_p2p.cpp
@@ -3,17 +3,36 @@
 #include <time.h>
 #include <cstdlib>
 
-using namespace std;
 
+using namespace std;
 int id, p;
 
-// Lecture 1 Exercise 3 : Blocking Point to Point Communication
-// =============================================================
+/*
+ Lecture 1 Exercise 3 : Blocking Point to Point Communication
+ =============================================================
+
+Write a program where every processor sends a piece of information to every 
+other processor using MPI_Send and MPI_Recv.
+
+a. The easiest, but slowest way to do this is to allow each process to have a turn to send data, 
+	with each other process waiting for that data (if you have 100 processes they will by waiting 99% 
+	of the time and only communicating 1% of the time on average). Implement this method.
+
+b. It is much quicker to allow every process to be sending or receiving data at the same time. 
+	You can either achieve this with clever ordering of the sends and receives or by using probe 
+	to handle communications as they come in (though you still need to watch out for potential 
+	blocking – We will be looking at non-blocking communications later. These are a bit more complex 
+	to implement, but remove the need to
+
+c. Order communications to avoid blocking. See if you can implement this using blocking communications. 
+   Don’t worry if it crashes when you first try, it is easy to get something wrong here!
+
+
+*/
+
 int main(int argc, char* argv[])
 {
-	// the goal is to have every processor speaking to every other processor.
-	// there are 3 different implementations of this going form basic to advanced ordering of communications.
-
+	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -21,10 +40,10 @@ int main(int argc, char* argv[])
 
 	int tag_num = 1;
 
-#if 0
+#if 1
 
-	// [Implementation A]: each process takes a turn at sending to other processors. 
-	// This is the slowest but easiest way of P2P communication.
+	// [Implementation (a)]: each process takes a turn at sending to other processors. 
+	// This is the slowest but easiest way of P2P blocking communication.
 
 	for (int s_id = 0; s_id != p; s_id++)
 	{
@@ -47,8 +66,8 @@ int main(int argc, char* argv[])
 		}
 		tag_num++;
 	}
-#elif 1
-	// [Implementation B]: Each process sending and receiving simultaneously. 
+#else
+	// [Implementation b & c]: Each process sending and receiving simultaneously. 
 	// Invovles ordering of sends/receives or using probes to handle communication.
 
 	int mid = p / 2;
@@ -93,9 +112,8 @@ int main(int argc, char* argv[])
 	}
 
 	tag_num++;
-#else 
-	// [Implementation C]: 
 
 #endif
 	MPI_Finalize();
+	return 0;
 }
