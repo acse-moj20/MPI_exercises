d4fbda794a6855f9e467997eec3bc0dffa8d2e5a
acse-moj20 <moj20@ic.ac.uk>
2021-04-05 10:26:05 +0100

    updated program

--- non_blocking_p2p.cpp
+++ non_blocking_p2p.cpp
@@ -1,8 +1,14 @@
 // Lecture 2 Exercise 1: Non Blocking P2P Communication
 // ====================================================
+/* Notes:
+	1. request variable has the information about a particular communication 
+	2. since we have a communication that is not copleting yet.
+	3. this section only sets up the communication. It doesnt wait for the communication to be completed.
+	4. it is only after the wait all that we can check that all communications have been completed.
+	5. You cant also reuse the variable that is being sent while in the communications set up. You can only re use it after the wait. 
+	6. It is perfectly acceptable to mix and match a blocking send/receive with a non-blocking send/receive.
 
-
-
+*/
 #include <mpi.h>
 #include <iostream>
 #include <time.h>
@@ -11,16 +17,15 @@
 
 using namespace std;
 int id, p;
-
 int num_recv_procs = 0;
-int* recv_procs = new int[num_recv_procs];
-int* send_procs = new int[2];
+std::vector<int> recv_procs;
+std::vector<int> send_procs;
 
-
+// Completes the non-blocking communication following our setup.
 void do_Comms(void) {
 
 	int tag_num = 1;
-	MPI_Request* request = new MPI_Request[(p - 1) * 2];
+	MPI_Request* request = new MPI_Request[num_recv_procs + 2];
 	double send_data[2];
 	double* recv_data = new double[num_recv_procs];
 
@@ -32,22 +37,16 @@ void do_Comms(void) {
 	}
 
 	for (int i = 0; i < 2; i++) {
-		send_data[i] = id * (i + 1) / 100.0;
+		send_data[i] = id * (i + 1) / 100.0; // data to be sent.
 		MPI_Isend(&send_data[i], 1, MPI_DOUBLE, send_procs[i], tag_num, MPI_COMM_WORLD, &request[cnt]); // this version of MPI send is the non blocking version.
 		cnt++;
 	}
 	MPI_Waitall(cnt, request, MPI_STATUSES_IGNORE);
-
+	tag_num++;
 	
 	cout << "Processor " << id << " sending ";
 	for (int i = 0; i < 2; i++) {
-		cout << send_data[i] << " to: " << send_procs[i] << "\t";
-	}
-
-	cout << "receiving: ";
-	for (int i = 0; i < num_recv_procs; i++) {
-		cout << recv_data[i] << "\t"
-			<< " from: " << recv_procs[i];
+		cout << send_data[i] << " to: " << send_procs[i] << ", ";
 	}
 
 	cout << endl;
@@ -57,14 +56,6 @@ void do_Comms(void) {
 
 }
 
-
-// Notes:
-	// request variable has the inofrmation about a particular communication 
-	// since we have a communication that is not copleting yet.
-	// this section only sets up the communication. It doesnt wait for the communication to be completed.
-	// it is only after the wait all that we can check that all communications have been completed.
-	// You cant also reuse the variable that is being sent while in the communications set up. You can only re use it after the wait. 
-	// It is perfectly acceptable to mix and match a blocking send/receive with a non-blocking send/receive.
 
 int main(int argc, char* argv[]) {
 
@@ -92,8 +83,8 @@ int main(int argc, char* argv[]) {
 
 	send_status[send_id[0]] = true;
 	send_status[send_id[1]] = true;
-	send_procs[0] = send_id[0]; 
-	send_procs[1] = send_id[1];
+	send_procs.push_back(send_id[0]);
+	send_procs.push_back(send_id[1]);
 
 	int cnt = 0;
 	// we set up all communications then do some potential calculations while waiting. All communications are checked afterwards for completion. 
@@ -113,27 +104,31 @@ int main(int argc, char* argv[]) {
 	
 	MPI_Waitall(cnt, request, MPI_STATUSES_IGNORE);
 	
+	
 	cout << "Processor " << id << " sending to: ";
 	for (int i = 0; i < p; i++) {
 		if (send_status[i]) { cout << i << " "; }
 	}
-	/*
-	cout << "receiving from: ";
+	
+	cout << " \treceiving from: ";
 	for (int i = 0; i < p; i++) {
 		if (recv_status[i]) {
 			cout << i << "\t";
-			num_recv_procs++;
-			recv_procs[num_recv_procs] = i;
 		}
-		cout << "\n" << id << " receiving " << num_recv_procs << " processes from: " << recv_procs[num_recv_procs];
-	}*/
+	}
 	cout << endl;
 	cout.flush();
-	
 
+	
+	for (int i = 0; i < p; i++) {
+		if (recv_status[i]) {
+			num_recv_procs++;
+			recv_procs.push_back(i);
+		}
+	}
 
 	//for (int i = 0; i < 10; i++) {
-	//do_Comms();
+	do_Comms();
 	//}
 
 	delete[] send_status;
